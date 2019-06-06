9ba73de1a89b48de07e8e43cd6578a42aa642634
Johann Knechtel <johann@nyu.edu>
2019-06-06 19:02:09 +0400

    added some DEF files as example for running the attack; minor edits script and gitignore

--- attack/all.sh
+++ attack/all.sh
@@ -4,12 +4,12 @@ BB_DIVISOR=8
 FILES="*/*.def"
 #FILES="DEF/original/*.def"
 
-## for 1st run; initialize RT files and split layer 1
-#for def in `ls $FILES`; do ./clean.sh $def; done
-#for def in `ls $FILES`; do ./batch.sh $def y $BB_DIVISOR 1; done
+# for 1st run; initialize RT files and split layer 1
+for def in `ls $FILES`; do ./clean.sh $def; done
+for def in `ls $FILES`; do ./batch.sh $def y $BB_DIVISOR 1; done
 
-# for repeating runs; just run on split layer 1
-for def in `ls $FILES`; do ./batch.sh $def n $BB_DIVISOR 1; done
+## for repeating runs; just run on split layer 1
+#for def in `ls $FILES`; do ./batch.sh $def n $BB_DIVISOR 1; done
 
 # remaining split layers
 for i in {2..4}
